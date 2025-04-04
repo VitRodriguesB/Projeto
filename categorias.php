24f2a7365735cfb0764e32db75c8018c2cfd2dd7
VitRodriguesB <vitornety2098@gmail.com>
2025-04-03 21:13:16 -0300

    update

--- categorias.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("cabecalho.php");
-?>
-<h1>Categorias</h1>
-<?php
-    require_once("rodape.php");
-?>
--- produtos.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once("cabecalho.php");
-?>
-<h1>Produtos</h1>
-<?php
-    require_once("rodape.php");
-?>
