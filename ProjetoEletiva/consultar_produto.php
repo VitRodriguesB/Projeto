69ec79ff2d2181ac71b28c20e3779d8991ef130e
VitRodriguesB <vitornety2098@gmail.com>
2025-04-03 21:54:46 -0300

    update final

---
+++ ProjetoEletiva/consultar_produto.php
@@ -0,0 +1,22 @@
+<?php 
+    require_once("cabecalho.php");
+?>
+
+<h2>Consultar Produto</h2>
+<div class="mb-2">
+    <p>Nome do Produto: <strong> Produto 1</strong></p>
+</div>
+
+<div class="mb-2">
+    <p>Descrição do produto: <strong>Produto1</strong></p>
+</div>
+
+<div class="mb-2">
+    <p>Valor do produto: <strong>Produto1</strong></p>
+</div>
+
+<div class="mb-2">
+    <button type="submit" class="btn btn-danger">Excluir</button>
+    <button type="button" class="btn btn-secondary"
+        onclick="history.back()">Voltar</button>
+</div>
---
+++ ProjetoEletiva/novo_produto.php
@@ -0,0 +1,35 @@
+<?php
+require_once("cabecalho.php");
+?>
+    <h2> Novo Produto </h2>
+    
+    <form method="post">
+                        
+                        <div class="mb-3">
+                            <label for="nome" class="form-label">Nome</label>
+                            <input type="text" id="nome" name="nome" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="des" class="form-label">Descrição</label>
+                            <input type="text" id="des" name="des" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="valor" class="form-label">valor</label>
+                            <input type="text" id="valor" name="valor" class="form-control" required="">
+                        </div>
+                    
+                        <div class="mb-3">
+                            <label for="acao" class="form-label">Ação</label>
+                            <input type="text" id="acao" name="acao" class="form-control" required="">
+                        </div>
+                    
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    <button type="button" class="btn-btn-secondary"
+                    onclick="history.back();">Voltar</button>
+                </form>
+            
+
+<?php
+  require_once("rodape.php");
